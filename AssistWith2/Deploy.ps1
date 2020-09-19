a9f2a43da8f1c0166c961f5b3a11255833d27e45
Tim Kopp <tim.kopp@cyber-balance.com>
2020-09-18 20:44:46 -0400

    updated ignore

--- AssistWith2/Deploy.ps1
+++
@@ -1,6 +0,0 @@
-## UPDATE AspNetCoreModuleV2
-$releaseto = "C:\inetpub\wwwroot\prod\AssistWith2\" 
-$webconf = $releaseto + "web.config" 
-$content = Get-Content -Path $webconf 
-$content = $content.Replace("AspNetCoreModule""","AspNetCoreModuleV2""")
-Set-Content -Path $webconf -Value $content  
